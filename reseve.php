9c57405f74341597d3a70a372a98aa5c28269984
SashaMelva <sa1524517@gmail.com>
2023-06-15 17:51:42 +0300

    tests rabbit

--- reseve.php
+++ reseve.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('jr-rabbitmq', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
 $channel->queue_declare('hello', false, false, false, false);
@@ -10,7 +10,9 @@ $channel->queue_declare('hello', false, false, false, false);
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
 $callback = function ($msg) {
-    echo ' [x] Received ', $msg->body, "\n";
+    echo ' [x] Received ', $msg->body * 2, "\n";
+    sleep(substr_count($msg->body, '.'));
+    echo " [x] Done\n";
 };
 
 $channel->basic_consume('hello', '', false, true, false, false, $callback);
@@ -18,5 +20,6 @@ $channel->basic_consume('hello', '', false, true, false, false, $callback);
 while ($channel->is_open()) {
     $channel->wait();
 }
+
 $channel->close();
 $connection->close();
